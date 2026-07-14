7bfaa2fc8d1458fd1b50a4c8c223bd1863e20853
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-14 23:11:55 +0400

    made a better temp mass output, still ass but ok for now ig

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ fn main() {
 
     if let Some(num) = args.query {
         println!("Returning for Atomic Number: {}", num);
-        println!("{}", output::mass::output(num));
+
+        if args.mass {
+            println!("Mass of Atom: \x1b[1m{} ({})\x1b[0m = {}", output::name::output(num), num, output::mass::output(num))
+        }
     }
 }
