f2c667b3a1e82c85b6e291a275d59694a72cd259
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-14 22:24:13 +0400

    Some demo code to test if it works

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ pub mod elements;
 pub mod config;
 
 fn main() {
+    let args = config::parse_flags();
 
+    if let Some(num) = args.number {
+        println!("Returning for Atomic Number: {}", num);
+    }
 }
-
