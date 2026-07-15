6eafce4b388856a69ae3a3b966d300144e2e1798
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-15 20:04:40 +0400

    added more tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::electron::quantum_atomic_model::{get_full_config, get_shorthand_config};
+
 pub mod details;
 pub mod cli;
 pub mod output;
@@ -12,5 +14,13 @@ fn main() {
         if args.mass {
             println!("Mass of Atom: \x1b[1m{} ({})\x1b[0m = {}", output::name::output(num), num, output::mass::output(num))
         }
+
+        if let Some(_string) = args.electron {
+            println!("Quantum Electronic Configuration: {}\n", get_full_config(num));
+
+            println!("Shorthand Configuration: {}", get_shorthand_config(num));
+        }
     }
+
+    return;
 }
