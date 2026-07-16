99e8e705645965be066c6c3839f54126a502f9c7
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-16 14:38:45 +0400

    Removed test

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,8 @@ use crate::electron::quantum_model::configuration::{get_full, get_semantic};
 
 fn main() {
     let args = cli::parse::parse_flags();
-
-    if let Some(num) = args.query {
-        println!("Returning for Atomic Number: {}", num);
-
-        if args.mass {
-            println!("Mass of Atom: \x1b[1m{} ({})\x1b[0m = {}", output::name::output(num), num, output::mass::output(num))
-        }
-
-        if let Some(_string) = args.electron {
-            println!("Number of Electrons: {:?}", output::electron::number(num));
-
-            println!("Quantum Electronic Configuration: {}\n", get_full(num));
-
-            println!("Shorthand Configuration: {}", get_semantic(num));
-        }
-    }
+    
+    
 
     return;
 }
