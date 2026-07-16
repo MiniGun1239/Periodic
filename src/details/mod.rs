a77649c91c823f15f175cb424f38eeace070c3ce
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-16 20:26:57 +0400

    changed logic to use json

--- src/details/mod.rs
+++ src/details/mod.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
-use serde::Deserialize;
+use serde_json;
 
 pub mod element;
 
 use element::Element;
+
+static RAW_JSON: &'static str = include_str!("element_info.json");
 
 pub fn lookup(atomic_number: u8) -> Option<Element> {
     let db = get_database();
@@ -12,30 +14,7 @@ pub fn lookup(atomic_number: u8) -> Option<Element> {
 
 
 pub fn get_database() -> HashMap<u8, Element> {
-    let mut table: HashMap<u8, Element> = HashMap::new();
-
-    table.insert(
-        1,
-        Element {
-            name: String::from("Hydrogen"),
-            atomic_number: 1,
-            mass: 1.008,
-            boiling_point: 0.0,
-            melting_point: 0.0,
-            density: 0.0,
-            phase: "".to_string(),
-            category: "".to_string(),
-            group: 0,
-            period: 0,
-            bohr_config: "".to_string(),
-            quantum_config: "".to_string(),
-            quantum_semantic_config: "".to_string(),
-            block: '',
-            first_ionization_energy: 0.0,
-            symbol: "".to_string(),
-            electron_affinity: 0.0,
-        }
-    );
+    let table: HashMap<u8, Element> = serde_json::from_str(RAW_JSON).unwrap();
 
     table
 }
