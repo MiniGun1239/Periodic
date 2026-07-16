b15b213800a12a2ebac4b7c2ebd9a1b86155a508
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-16 20:29:02 +0400

    removed useless deps

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ pub mod cli;
 pub mod output;
 pub mod electron;
 
-use crate::electron::quantum_model::configuration::{get_full, get_semantic};
-
 fn main() {
     let args = cli::parse::parse_flags();
 
