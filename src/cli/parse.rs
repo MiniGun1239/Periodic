b2464a013051b8cde92429c8b022b73a586cd104
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-16 20:25:20 +0400

    explicitly added type declaration

--- src/cli/parse.rs
+++ src/cli/parse.rs
@@ -2,6 +2,6 @@ use clap::Parser;
 use crate::cli::args_struct::CliArgs;
 
 pub fn parse_flags() -> CliArgs {
-    let args = CliArgs::parse();
+    let args: CliArgs = CliArgs::parse();
     args
 }
