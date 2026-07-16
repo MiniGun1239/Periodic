28f6768284ccc7b10026b2f76105e36f4f77a581
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-16 23:27:06 +0400

    added electron.rs

--- src/cli/handle_flag/mod.rs
+++ src/cli/handle_flag/mod.rs
@@ -3,25 +3,28 @@ use crate::cli::cli_args::CliArgs;
 pub mod name;
 pub mod symbol;
 pub mod mass;
+pub mod electron;
 
 pub fn handle(args: CliArgs) {
     if args.query == None {
         todo!()
     }
-    
+
     let number: u8 = args.query.unwrap();
-    
+
     if args.name {
         name::parse(number);
     }
-    
+
     if args.symbol {
         symbol::parse(number);
     }
-    
+
     if args.mass {
         mass::parse(number);
     }
-    
-    
+
+    if args.electron != None {
+        electron::parse(args.electron.unwrap().to_lowercase());
+    }
 }
