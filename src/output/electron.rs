c845c3f55d3192e0ce382963b6abad642c455100
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-17 17:03:41 +0400

    early rendition of output

--- src/output/electron.rs
+++ src/output/electron.rs
@@ -6,3 +6,27 @@ pub fn number(atomic_number: u8) {
 
     element.unwrap().atomic_number;
 }
+
+pub fn quantum(number: u8) {
+    let element: Option<Element> = lookup(number);
+
+    element.unwrap().quantum_config;
+}
+
+pub fn bohr(number: u8) {
+    let element: Option<Element> = lookup(number);
+
+    element.unwrap().bohr_config;
+}
+
+pub fn semantic(number: u8) {
+    let element: Option<Element> = lookup(number);
+
+    element.unwrap().quantum_semantic_config;
+}
+
+pub fn valence(number: u8) {
+    let element: Option<Element> = lookup(number);
+
+    drop(element);
+}
