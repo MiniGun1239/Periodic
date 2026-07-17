d5295f4b1e1931893c45e607f3c9f24288c6cdec
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-17 17:05:02 +0400

    more frameworking

--- src/cli/handle_flag/electron.rs
+++ src/cli/handle_flag/electron.rs
@@ -1,59 +1,110 @@
-pub fn parse(args: String) {
+use crate::output::electron;
+use crate::output::electron::{number, quantum};
 
-    let bohr_config = [
-        "b", "bohr", "bohr-config",
-        "-b", "--bohr", "--bohr-config",
-    ];
+static BOHR_CONFIG_KEYWORDS: [&str; 3] = [
+    "bc", "bohr", "bohr-config"
+];
 
-    let quantum_config = [
-        "q", "quantum-config", "quantum",
-        "-q", "--quantum-config", "--quantum"
-    ];
+static QUANTUM_CONFIG_KEYWORDS: [&str; 3] = [
+    "qc", "quantum-config", "quantum"
+];
 
-    let valence = [
-        "v", "valence-electron", "valency",
-        "-v", "--valence-electron", "--valency"
-    ];
+static SEMANTIC_CONFIG_KEYWORD: [&str; 7] = [
+    "sc", "semantic-config", "semantic",
+    "nc", "noble-config", "noble-gas-config", "noblegas-config"
+];
 
-    if args == "-h" || args == "--help" {
-        todo!()
+static VALENCE_KEYWORDS: [&str; 3] = [
+    "v", "valence-electron", "valency"
+];
+
+static FIRST_IONIZATION_ENERGY_KEYWORDS: [&str; 5] = [
+    "first-ionization-energy", "ionization-energy", "ionization",
+    "fie", "ie"
+];
+
+static ELECTRON_AFFINITY_KEYWORDS: [&str; 3] = [
+    "electron-affinity", "affinity", "ea",
+];
+
+pub fn parse(number: u8, args: String) {
+
+    if args == "-h" || args == "--help" || args == "h" || args == "help" {
+        help()
     }
 
     else if args == "default" {
-        todo!()
-    }
-
-    else if bohr_config.contains(&&*args) {
-        todo!()
-    }
-
-    else if quantum_config.contains(&&*args) {
-        todo!()
-    }
-
-    else if args == "config" || args == "--config" {
         todo!(
-            "add both the bohr and quantum and semantic"
+            "call literally everything"
         )
     }
 
-    else if valence.contains(&&*args) {
-        todo!()
+    else if BOHR_CONFIG_KEYWORDS.contains(&&*args) {
+        bohr_config(number);
+    }
+
+    else if QUANTUM_CONFIG_KEYWORDS.contains(&&*args) {
+        quantum_config(number);
+    }
+
+    else if SEMANTIC_CONFIG_KEYWORD.contains(&&*args) {
+        semantic_config(number);
+    }
+
+    else if args == "config" || args == "--config" {
+        bohr_config(number);
+        quantum_config(number);
+    }
+
+    else if VALENCE_KEYWORDS.contains(&&*args) {
+        valence(number);
     }
 }
 
 fn help() {
-    todo!("add help things")
+    todo!(
+        "print all the config functions"
+    )
 }
 
 fn incomplete() {
-    todo!("FAHHH")
+    todo!(
+        "print a warning, then help, then everything"
+    )
 }
 
 fn bohr_config(atomic_number: u8) {
-    todo!()
+    todo!(
+        "call output::electron::bohr"
+    )
 }
 
 fn quantum_config(atomic_number: u8) {
-    todo!()
+    todo!(
+        "call output::electron::quantum"
+    )
+}
+
+fn semantic_config(atomic_number: u8) {
+    todo!(
+        "call output::electron::semantic"
+    )
+}
+
+fn valence(atomic_number: u8) {
+    todo!(
+        "call output::electron::valence"
+    )
+}
+
+fn ionization_energy(atomic_number: u8) {
+    todo!(
+        "call output::electron::ionize_energy"
+    )
+}
+
+fn affinity(atomic_number: u8) {
+    todo!(
+        "call output::electron::affinity"
+    )
 }
