2cfc1e53e06ec7b09286b9fa2774f0d38912ffa3
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-18 16:20:39 +0400

    refactoring

--- src/electron/mod.rs
+++ src/electron/mod.rs
@@ -1,2 +1,3 @@
 pub mod bohr_atomic_model;
 pub mod quantum_model;
+pub mod valence_electrons;
--- src/electron/quantum_model/mod.rs
+++ src/electron/quantum_model/mod.rs
@@ -1,4 +1,3 @@
 pub mod configuration;
 pub mod orbitals;
-pub mod valence_electrons;
 pub mod noble_gases;
