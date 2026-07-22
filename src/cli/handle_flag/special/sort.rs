9f1da1236aeed6c8ee1131b035d2183a22cf988b
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-22 20:38:14 +0400

    refactoring to match

--- src/cli/handle_flag/special/sort.rs
+++ src/cli/handle_flag/special/sort.rs
@@ -36,30 +36,3 @@ fn incomplete() {
         "print incomplete, then call help"
     )
 }
-
-
-// output functions
-
-fn out_name(reverse: bool) {
-    todo!(
-        "call output::special::sort::name or name_reversed"
-    )
-}
-
-fn out_symbol(reverse: bool) {
-    todo!(
-        "call output::special::sort::symbol or symbol_reversed"
-    )
-}
-
-fn out_number(reverse: bool) {
-    todo!(
-        "call output::special::sort::number or number_reversed"
-    )
-}
-
-fn out_mass(reverse: bool) {
-    todo!(
-        "call output::special::special::sort::mass_reverse"
-    )
-}
