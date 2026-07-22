91a029ad01fb557e453151cbcb1fa64670458ef2
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-22 20:41:56 +0400

    refactoring to not use duplicated code

--- src/output/special/sort.rs
+++ src/output/special/sort.rs
@@ -95,3 +95,16 @@ fn out_mass(reverse: bool) {
         println!("{}: {}", name, mass);
     }
 }
+
+
+// helper
+
+fn sort_by_name_with_index(elements: Vec<Element>) -> Vec<(u8, String)> {
+    let out = elements
+        .iter()
+        .enumerate()
+        .map(|(index, element)| ((index + 1) as u8, element.name.clone()))
+        .collect();
+
+    out
+}
