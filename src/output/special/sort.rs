f2b6a7467e6f2b863e21bf6f005589d6811f05f8
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-22 20:14:39 +0400

    refactored to match

--- src/output/special/sort.rs
+++ src/output/special/sort.rs
@@ -48,3 +48,30 @@ pub fn mass_reversed() {
         "Sort all elements by mass in descending order"
     )
 }
+
+
+// output functions
+
+fn out_name(reverse: bool) {
+    todo!(
+        "call output::special::sort::name or name_reversed"
+    )
+}
+
+fn out_symbol(reverse: bool) {
+    todo!(
+        "call output::special::sort::symbol or symbol_reversed"
+    )
+}
+
+fn out_number(reverse: bool) {
+    todo!(
+        "call output::special::sort::number or number_reversed"
+    )
+}
+
+fn out_mass(reverse: bool) {
+    todo!(
+        "call output::special::special::sort::mass_reverse"
+    )
+}
