840d268b8a3b641c62b966cb78f48f67b9d861c6
Amaan <amaanarmaanmuhammad@gmail.com>
2026-07-23 13:31:07 +0400

    added new usage fn

--- src/cli/handle_flag/regular/name.rs
+++ src/cli/handle_flag/regular/name.rs
@@ -3,3 +3,7 @@ use crate::output::regular::name::output;
 pub fn parse(atomic_number:u8) {
     output(atomic_number);
 }
+
+pub fn usage() {
+    println!("Usage: periodic <atomic_number> --name")
+}
